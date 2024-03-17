61105ceba7c9308f1bbfcb625736e720b9dede1b
Michael <enjoyweaver@gmail.com>
2024-03-16 22:30:29 -0500

    tailwind fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 
 const root = document.getElementById("root");
 
@@ -8,6 +9,6 @@ if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   );
 }
