5a61c327a7b407a32eed0e86bc4c2a3bc440d0db
Michael <enjoyweaver@gmail.com>
2024-03-16 21:18:00 -0500

    banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "@farcaster/auth-kit/styles.css";
 import { AuthKitProvider, SignInButton, useProfile } from "@farcaster/auth-kit";
 import DryRunButton from "./DryRunButton";
+import styles from "./App.module.css"; // Importing the CSS module
 
 const config = {
   // For a production app, replace this with an Optimism Mainnet
@@ -13,14 +14,23 @@ const config = {
 
 function App() {
   return (
-    <main style={{ fontFamily: 'Inter, "Inter Placeholder", sans-serif' }}>
+    <main className={styles.app}>
+      {" "}
+      {/* Applying the black background */}
       <AuthKitProvider config={config}>
         <div style={{ position: "fixed", top: "12px", right: "12px" }}>
           <SignInButton />
         </div>
+        <div className={styles.center}>
+          <img
+            src="/banner.jpg"
+            alt="Banner"
+            style={{ width: "75%", height: "auto", margin: "auto" }}
+          />
+        </div>
         <div style={{ paddingTop: "20vh", textAlign: "center" }}>
-          <h1>Planet Keeper</h1>
-          <p>
+          <h1 className={styles.whiteText}>Planet Keeper</h1>
+          <p className={styles.whiteText}>
             An app for our pollinators, utilizing Arweave, ao, and Farcaster.
           </p>
           <Profile />
@@ -41,16 +51,16 @@ function Profile() {
     <>
       {isAuthenticated ? (
         <div>
-          <p>
+          <p className={styles.whiteText}>
             Hello, {displayName}! Your FID is {fid}.
           </p>
-          <p>
+          <p className={styles.whiteText}>
             Your custody address is: <pre>{custody}</pre>
           </p>
           <DryRunButton />
         </div>
       ) : (
-        <p>
+        <p className={styles.whiteText}>
           Click the "Sign in with Farcaster" button above, then scan the QR code
           to sign in.
         </p>
